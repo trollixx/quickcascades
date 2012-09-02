cc54f258bd68e83611d62b540d453b81be07e0df
Oleg Shparber <trollixx@gmail.com>
2012-09-02 22:39:21 +0300

    cookbook: do not use resources

--- examples/cookbook/main.cpp
+++ examples/cookbook/main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
     //qputenv("QML_IMPORT_TRACE", "1");
 
     QDeclarativeView *m_view = new QDeclarativeView();
+    QObject::connect(m_view->engine(), SIGNAL(quit()), &app, SLOT(quit()));
 
     m_view->engine()->addPluginPath("../../build");
 
@@ -45,18 +46,20 @@ int main(int argc, char *argv[])
     //m_view->viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
     //m_view->viewport()->setAttribute(Qt::WA_NoSystemBackground);
 
-    m_view->setSource(QUrl("qrc:/qml/main.qml"));
-    //m_view->setSource(QUrl("qml/main.qml"));
+    //m_view->setSource(QUrl("qrc:/qml/main.qml"));
+    m_view->setSource(QUrl("assets/main.qml"));
     m_view->setResizeMode(QDeclarativeView::SizeRootObjectToView);
 
     /* Prepare the QML view and load the game content */
     /*m_view->setSource(QUrl("qrc:qml/main.qml"));
     m_view->setResizeMode(QDeclarativeView::SizeRootObjectToView);*/
-    //m_view->showFullScreen();
-    m_view->resize(1280, 720);
-    m_view->show();
 
-    QObject::connect(m_view->engine(), SIGNAL(quit()), &app, SLOT(quit()));
+#ifdef Q_OS_BLACKBERRY
+    m_view->showFullScreen();
+#else
+    m_view->resize(720, 1280);
+    m_view->show();
+#endif
 
     return app.exec();
 }
