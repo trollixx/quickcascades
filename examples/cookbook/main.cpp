9005b194cc30d209cf193ecd44509e46bd145580
Oleg Shparber <trollixx@gmail.com>
2012-10-05 01:37:02 +0300

    cookbook: removed useless OpenGL related code

--- examples/cookbook/main.cpp
+++ examples/cookbook/main.cpp
@@ -3,13 +3,9 @@
 #include <QtDeclarative>
 
 #include <QDir>
-//#include <QGLWidget>
-//#include <QGLFormat>
 
 int main(int argc, char *argv[])
 {
-    //QApplication::setGraphicsSystem("opengl");
-
     QApplication app(argc, argv);
 
     //qputenv("QML_IMPORT_TRACE", "1");
@@ -19,40 +15,8 @@ int main(int argc, char *argv[])
 
     m_view->engine()->addPluginPath("../../build");
 
-    /* GL viewport increases performance on blackberry
-    QGLFormat format = QGLFormat::defaultFormat();
-    format.setSampleBuffers(false);
-    QGLWidget *glWidget = new QGLWidget(format);
-    glWidget->setAutoFillBackground(false);
-
-    view->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
-    view->setAttribute(Qt::WA_OpaquePaintEvent);
-    view->setAttribute(Qt::WA_NoSystemBackground);
-    view->viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
-    view->viewport()->setAttribute(Qt::WA_NoSystemBackground);*/
-
-    //QGLFormat format = QGLFormat::defaultFormat();
-    //format.setSampleBuffers(false);
-
-    //QGLWidget *glWidget = new QGLWidget(format);
-    // Comment the following line if you get display problems
-    // (usually when the top-level element is an Item and not a Rectangle)
-    //glWidget->setAutoFillBackground(false);
-    //m_view->setViewport(glWidget);
-
-    //m_view->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
-    //m_view->setAttribute(Qt::WA_OpaquePaintEvent);
-    //m_view->setAttribute(Qt::WA_NoSystemBackground);
-    //m_view->viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
-    //m_view->viewport()->setAttribute(Qt::WA_NoSystemBackground);
-
-    //m_view->setSource(QUrl("qrc:/qml/main.qml"));
     m_view->setSource(QUrl("assets/main.qml"));
     m_view->setResizeMode(QDeclarativeView::SizeRootObjectToView);
-
-    /* Prepare the QML view and load the game content */
-    /*m_view->setSource(QUrl("qrc:qml/main.qml"));
-    m_view->setResizeMode(QDeclarativeView::SizeRootObjectToView);*/
 
 #ifdef Q_OS_BLACKBERRY
     m_view->showFullScreen();
